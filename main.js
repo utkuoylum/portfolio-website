0ee7aa37d8be589031c71e2a60b9cd4dda474b7a
Etem Utku Oylum <utkuoylum@gmail.com>
2023-08-10 06:49:16 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 document.querySelector("#toggle-theme").addEventListener("click", () => {
-    document.documentElement.classList.toggle("light-theme")
-})
+  document.documentElement.classList.toggle("light-theme");
+  document.querySelector("#git-logo").classList.toggle("git-light");
+});
 
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-const scrollDown = document.querySelector("#scroll-down")
+const scrollDown = document.querySelector("#scroll-down");
 
-if(prefersLight) {
-    document.documentElement.classList.add("light-theme");
+if (prefersLight) {
+  document.documentElement.classList.add("light-theme");
 }
 
 scrollDown.addEventListener("click", () => {
-    window.scrollTo({
-        top: document.querySelector("#about-me").offsetTop - 20,
-    })
-})
+  window.scrollTo({
+    top: document.querySelector("#about-me").offsetTop - 20,
+  });
+});
